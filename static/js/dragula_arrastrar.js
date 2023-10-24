8b3ee82e26fae8d309db7dfa38b5a10b184fa7fd
Luis Molina <Luis.Molina1@alu.uclm.es>
2023-10-24 13:00:53 +0200

    Implementación drag-archivo +  Boostrap
    - Implementación Boostrap
    - Estructuración del proyecto
    - Mejorada la carga de scripts

---
+++ static/js/dragula_arrastrar.js
@@ -0,0 +1,29 @@
+// Evitar comportamiento por defecto al arrastrar sobre el dropzone
+document.getElementById('dropzone').addEventListener('dragover', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+});
+
+// Evitar comportamiento por defecto al soltar en el dropzone
+document.getElementById('dropzone').addEventListener('drop', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    let file = e.dataTransfer.files[0];
+    if (file) {
+        document.getElementById('fileInput').files = e.dataTransfer.files;
+        document.getElementById('dropzone').innerText = "Archivo seleccionado: " + file.name;
+    }
+});
+
+// Evento al hacer clic en el contenedor de arrastrar y soltar
+document.getElementById('dropzone').addEventListener('click', function() {
+    document.getElementById('fileInput').click();
+});
+
+// Evento al seleccionar un archivo usando el input de archivos
+document.getElementById('fileInput').addEventListener('change', function() {
+    if (this.files.length > 0) {
+        document.getElementById('dropzone').innerText = "Archivo seleccionado: " + this.files[0].name;
+    }
+});
